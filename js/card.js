7d4d0940041366c6fa31af78fb5fe9f14d020845
Jesús Hernández <cjesus02hdz@gmail.com>
2022-06-22 20:24:57 -0500

    card renderizadas por el for

--- js/card.js
+++ js/card.js
@@ -2,7 +2,8 @@ const users = [
     {
         name: 'Jesús',
         lastname: 'Hernández',
-        card: {
+        avatar: '', 
+        cards: [{
             banco: 'devfbank',
             numCard: 555444333222111,
             saldo: 5766,
@@ -12,21 +13,71 @@ const users = [
                 referencia: 476768,
                 saldo: 567
             }
+        },
+        {
+            banco: 'bancomer',
+            numCard: 555444333222000,
+            saldo: 5766,
+            pinAtm: 4657,
+            retiroSinTarjeta: {
+                habilitado: true,
+                referencia: 476768,
+                saldo: 567
+            }
         }
+        ]
     },
     {
         name: 'Carlos',
         lastname: 'Slim',
-        card: {
-            banco: 'bancomer',
-            numCard: 555444333222555,
-            saldo: 5776766,
-            pinAtm: 4455,
+        avatar: '',
+        cards: [{
+                banco: 'bancomer',
+                numCard: 555444333222555,
+                saldo: 5776766,
+                pinAtm: 4455,
+                retiroSinTarjeta: {
+                    habilitado: false,
+                    referencia: null,
+                    saldo: null
+                }
+            }
+        ]
+    },
+    {
+        name: 'Pedro',
+        lastname: 'Smit',
+        avatar: '', 
+        cards: [{
+            banco: 'devfbank',
+            numCard: 555444333222111,
+            saldo: 5766,
+            pinAtm: 4657,
             retiroSinTarjeta: {
-                habilitado: false,
-                referencia: null,
-                saldo: null
+                habilitado: true,
+                referencia: 476768,
+                saldo: 567
+            }
+        },
+        {
+            banco: 'bancomer',
+            numCard: 555444333222000,
+            saldo: 5766,
+            pinAtm: 4657,
+            retiroSinTarjeta: {
+                habilitado: true,
+                referencia: 476768,
+                saldo: 567
             }
         }
+        ]
     }
 ]
+
+// console.log(users[0].cards)
+// for (let index = 0; index < users.length; index++) {
+//     console.log(`Usuario: ${users[index].name} - Num. Tarjeta: `);
+//     for (let j = 0; j < users[index].cards.length; j++) {
+//         console.log(`${users[index].cards[j].numCard} - PIN: ${users[index].cards[j].pinAtm}`)
+//     }
+// }
