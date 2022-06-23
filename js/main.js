7f55fcd171c93a095008ba5044a01e872e1d6e3c
Jesús Hernández <cjesus02hdz@gmail.com>
2022-06-22 21:02:54 -0500

    Login initial

--- js/main.js
+++ js/main.js
@@ -8,7 +8,14 @@ const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()]
 
 const atm = document.getElementById('rootAtm');
 const div = document.createElement('div');
-const botonesPublic = `
+
+const limpiarCajero = () => {
+    const divClear = ` `;
+    atm.innerHTML = divClear;
+}
+
+const retiroSinTarjeta = () => {
+    const botonesPublic = `
     <div class="col-sm-12">
         <h2 class="text-black text-center">Bienvenido</h2>
         <div id="divDate">
@@ -19,12 +26,17 @@ const botonesPublic = `
         <div class="d-grid gap-2 col-10 mx-auto">
             <a href="#" class="btn btn-primary">Retirar sin Tarjeta</a>
             <a href="#" class="btn btn-primary">Ingresar una tarjeta</a>
-            <a href="#" class="btn btn-danger" data-bs-dismiss="modal">Salir</a>
+            <a href="#" class="btn btn-danger" data-bs-dismiss="modal" onclick="limpiarCajero();">Salir</a>
         </div>
     </div>
 `
 div.innerHTML = botonesPublic
 atm.appendChild(div);
+const botonA = document.getElementById('botonA')
+}
+
+
+
 
 
 //  -   Renderizar Tarjetas
@@ -32,11 +44,8 @@ atm.appendChild(div);
 const cardsDiv = document.getElementById('cardsDiv');
 
 for (let index = 0; index < users.length; index++) {
-    var cardSelect = () => {
-        alert(`Card de ${users[index].name}`)
-    }
     var card = `
-        <div class="card" id="card${index}" aria-hidden="true" onclick="cardSelect()">
+        <div class="card" id="card${index}" aria-hidden="true" data-bs-toggle="modal" data-bs-target="#cajero" onclick="viewLogin();">
             <div class="card-body">
                 <h5 class="card-title placeholder-glow">
                 <span class="placeholder col-6"></span>
@@ -58,4 +67,13 @@ for (let index = 0; index < users.length; index++) {
     // for (let j = 0; j < users[index].cards.length; j++) {
     //     console.log(`${users[index].cards[j].numCard} - PIN: ${users[index].cards[j].pinAtm}`)
     // }
+}
+
+//  -   LOGIN
+
+const viewLogin = () => {
+    var uiLogin = `
+    <p>Login</p>
+    `
+    atm.innerHTML = uiLogin;
 }
